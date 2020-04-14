a1a89ff24345e2a542448009c6a33dee9f514a02
VKA87 <valentin.assassi@gmail.com>
2020-04-14 20:10:16 +0100

    api updated

--- api.py
+++ api.py
@@ -4,8 +4,9 @@ from gol.animation import create_animation
 
 
 if __name__ =='__main__':
-    init_state = np.zeros((100, 100))
+    init_state = np.zeros((100, 100)).astype(int)
     init_state[51, 49:51] = 1
     init_state[50, 48:50] = 1
     init_state[49, 49] = 1
-    anim = create_animation(init_state, speed=100)
+    anim = create_animation(init_state, interval=200, n_frames=1000)
+    anim.save('gol.mp4')
